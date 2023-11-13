ce9ca2b37b9718443b2f95b85c1c11e5e590de0e
2901shivam <shivamsingh201617@gmail.com>
2023-11-13 15:07:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Day6
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OurClass Hr = new OurClass();
+            Hr.display(workingHours: 8, workingdays: 30);
+            OurClass Admin = new OurClass();
+            Admin.display(workingHours: 8, workingdays: 30, projecthandel: 3);
+            OurClass SoftwareDeveloper = new OurClass();
+            SoftwareDeveloper.display(workingHours: 8, workingdays: 30, projecthandel: 4, extra: 5);
+        }
+    }
+}
